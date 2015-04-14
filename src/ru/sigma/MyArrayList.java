b5b7790eb8f9384abad8569dd9a9370fe73b7f84
Аня <klann@inbox.ru>
2015-04-14 15:35:56 +0300

    Update MyArrayList.java

--- src/ru/sigma/MyArrayList.java
+++ src/ru/sigma/MyArrayList.java
@@ -46,8 +46,40 @@ public class MyArrayList {
     }
 
 
+    public void insertArray(int [] elements, int index){
+            for (int j = 0; j < elements.length/2 ; j++) {
+                int temp = elements[j];
+                elements[j] = elements[elements.length-1-j];
+                elements[elements.length-1-j]=temp;
+            }
+        for (int i = 0; i < elements.length; i++) {
+            insert(elements[i], index);
+        }
+    }
+
+
     public void remove(int index){
+        if (index>=0 && index< size) {
+            int[] newArray = new int[size - 1];
+            for (int i = 0; i < index; i++) {
+                newArray[i] = objects[i];
+            }
+            for (int i = index; i < size-1 ; i++) {
+                newArray[i] = objects[i+1];
+            }
+            objects = newArray;
+            size--;
+        }
 
     }
 
+
+    public void clear (){
+        int[] newArray = new int[0];
+        size=0;
+        objects = newArray;
+
+
+
+    }
 }
